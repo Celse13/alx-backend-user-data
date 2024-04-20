7062b6077b213cf1daf21f39ffd17b33c6f9f988
celse13 <rugiracelse13@gmail.com>
2024-04-20 21:32:41 +0200

    Returning the cookie value

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Module of auth"""
 from typing import List, TypeVar
 from flask import request
+from os import getenv
 
 
 class Auth:
@@ -25,3 +26,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar("User"):
         """current user"""
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request"""
+        if request is None:
+            return None
+        session_name = getenv("SESSION_NAME")
+        return request.cookies.get(session_name)
