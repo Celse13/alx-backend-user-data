35caa16fef6bdd8f95cbffbea4257ec5ec5eb288
celse13 <rugiracelse13@gmail.com>
2024-04-20 23:36:28 +0200

    Implementing logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -32,3 +32,14 @@ def session_login():
     response = make_response(jsonify(user.to_json()))
     response.set_cookie(os.getenv('SESSION_NAME'), session_id)
     return response
+
+
+@app_views.route(
+    '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """DELETE /auth_session/logout"""
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+    else:
+        return jsonify({}), 200
