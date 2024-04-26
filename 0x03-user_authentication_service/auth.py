03d42c3261d56cd80a76b987de72ed9140020125
celse13 <rugiracelse13@gmail.com>
2024-04-26 22:21:21 +0200

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,4 +83,4 @@ class Auth:
             self._db.update_user(user.id, hashed_password=hashed_password,
                                  reset_token=None)
         except NoResultFound:
-            raise ValueError("Reset token not found")
+            raise ValueError
