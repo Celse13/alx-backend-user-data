d7ff6b08c6c5feada0af96280cab5e9c3bbdba83
celse13 <rugiracelse13@gmail.com>
2024-04-26 22:40:25 +0200

    Updating password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,7 +74,7 @@ def get_reset_password():
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
-    """Update password"""
+    """Update password """
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
