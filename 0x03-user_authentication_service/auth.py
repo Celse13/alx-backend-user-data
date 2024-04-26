2d775ea480761b2b7004bfaf6f13ec8bc1deeccf
celse13 <rugiracelse13@gmail.com>
2024-04-26 22:17:16 +0200

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,11 +76,11 @@ class Auth:
             raise ValueError("User doesn't exist")
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """ Update a user's password """
+        """Update a user's password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
             self._db.update_user(user.id, hashed_password=hashed_password,
                                  reset_token=None)
         except NoResultFound:
-            raise ValueError("Token doesn't exist")
+            raise ValueError("Reset token not found")
